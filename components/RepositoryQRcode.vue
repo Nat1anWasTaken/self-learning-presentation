605aedd4a77f6541429fad066305d5dcb1228a4c
Nat1anWasTaken <nathan@nat1an.xyz>
2024-06-03 11:44:24 +0800

    feat(components/RepositoryQRcode.vue): add Vue component for generating QR code

---
+++ components/RepositoryQRcode.vue
@@ -0,0 +1,25 @@
+<template>
+  <vue-qrcode
+    value="https://github.com/Nat1anWasTaken/self-learning-presentation"
+    @change="onDataUrlChange"
+  />
+</template>
+<script>
+import VueQrcode from 'vue-qrcode'
+
+export default {
+  data() {
+    return {
+      dataUrl: null,
+    }
+  },
+  components: {
+    VueQrcode,
+  },
+  methods: {
+    onDataUrlChange(dataUrl) {
+      this.dataUrl = dataUrl
+    },
+  },
+}
+</script>
